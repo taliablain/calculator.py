3eebf248d6af88a91a85cd2443a9cac5b946a266
Talia Blain <tb1126@york.ac.uk>
2022-02-15 15:34:55 +0000

    Initial commit

---
+++ boolean.py
@@ -0,0 +1,13 @@
+import struct
+
+#name = ' '
+#name = input("Type in your name: ")
+#if name:
+    #print(name)
+#else:
+    #print("Nothing")
+
+mylist = ['talia', 'hello', 'hi']
+for position, talia in enumerate(mylist):
+    mylist[position] = talia+talia
+print(mylist)
